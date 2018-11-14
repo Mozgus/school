df2e1df52815d1f839512988020566540aaf7412
Mozgus <retailnet@rambler.ru>
2018-11-15 01:25:07 +0300

    Run yii application with config

--- web/index.php
+++ web/index.php
@@ -1,1 +1,10 @@
-Welcome 
+<?php
+define('YII_ENV', 'dev');
+define('YII_DEBUG', true);
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+$config = require __DIR__ . '/../config/web.php';
+try {
+    $yii = new yii\web\Application($config);
+} catch (\yii\base\InvalidConfigException $e) {
+}
+$yii->run();
