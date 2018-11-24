b561ff11bcc46c29c65f9ca10c268fb1cc08c2c6
Mozgus <retailnet@rambler.ru>
2018-11-24 11:35:50 +0300

    Add migration for user table

--- config/console.php
+++ config/console.php
@@ -1,5 +1,14 @@
 <?php
 return [
     'id' => 'school-console',
-    'basePath' => dirname(__DIR__)
+    'basePath' => dirname(__DIR__),
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=mysql',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ]
+    ]
 ];
