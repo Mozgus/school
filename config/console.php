638492533ba7a9800a0f390481e855b097aadafe
Mozgus <retailnet@rambler.ru>
2018-11-24 13:25:51 +0300

    Database connection

--- config/console.php
+++ config/console.php
@@ -3,12 +3,6 @@ return [
     'id' => 'school-console',
     'basePath' => dirname(__DIR__),
     'components' => [
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=mysql',
-            'username' => 'root',
-            'password' => '',
-            'charset' => 'utf8',
-        ]
+        'db' => require __DIR__ . '/db.php'
     ]
 ];
