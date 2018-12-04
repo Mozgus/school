16c20a33df8de946246cd372823db9b1f3f1a3d3
Mozgus <retailnet@rambler.ru>
2018-12-04 23:06:42 +0300

    Login with check e-mail and password

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -46,8 +46,22 @@ class UserController extends Controller
 
     public function actionLogin()
     {
+        if (Yii::$app->request->isPost)
+            return $this->actionLoginPost();
         $userLoginForm = new UserLoginForm();
         return $this->render('login', compact('userLoginForm'));
+    }
+
+    public function actionLoginPost()
+    {
+        $userLoginForm = new UserLoginForm();
+        if ($userLoginForm->load(Yii::$app->request->post()))
+            if ($userLoginForm->validate())
+            {
+                $userLoginForm->login();
+                return $this->redirect('/');
+            }
+        return $this->render('login',compact('userLoginForm'));
     }
 
     public function actionLogout()
--- models/UserLoginForm.php
+++ models/UserLoginForm.php
@@ -2,6 +2,7 @@
 
 namespace app\models;
 
+use yii;
 use yii\base\Model;
 
 class UserLoginForm extends Model
@@ -14,7 +15,37 @@ class UserLoginForm extends Model
         return [
             ['email', 'required'],
             ['password', 'required'],
-            ['email', 'email']
+            ['email', 'email'],
+            ['email', 'errorIfEmailNotFound'],
+            ['password', 'errorIfPasswordWrong']
         ];
     }
+
+    public function errorIfEmailNotFound()
+    {
+        $userRecord = UserRecord::findUserByEmail($this->email);
+        if ($userRecord->email != $this->email) {
+            $this->addError('email',
+                'This e-mail does not registered');
+        }
+    }
+
+    public function errorIfPasswordWrong()
+    {
+        if ($this->hasErrors())
+            return;
+        $userRecord = UserRecord::findUserByEmail($this->email);
+        if ($userRecord->passhash != $this->password)
+            $this->addError('password', 'Wrong password');
+    }
+
+    public function login()
+    {
+        if ($this->hasErrors())
+            return;
+        $userRecord = UserRecord::findUserByEmail($this->email);
+        $userIdentity = UserIdentity::findIdentity($userRecord->id);
+        Yii::$app->user->login($userIdentity);
+    }
+
 }
